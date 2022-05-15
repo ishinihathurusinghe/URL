d162d2d3115a6e90613b2f73676f24fc31a867db
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-05-15 11:26:18 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+     <title> Home  </title>
+</head>
+
+<body>
+     <h1> Home Page </h1>
+
+    <a href=" news.php? news_item=32"> read news item </a>
+</body>
+</html>
---
+++ news.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+     <title> news  </title>
+</head>
+
+<body>
+     <h1> news Page </h1>
+
+     <?php 
+     
+       print_r($_GET);
+     
+     ?>
+</body>
+</html>
